f972580e691006b6ac67d126f8b97d30ab9ddf4f
ard14n <ardianj4@gmail.com>
2017-06-13 20:08:11 +0200

    10.06 11:22

---
+++ app/src/main/java/com/abmedia/hsfuldapp/Grade.java
@@ -0,0 +1,24 @@
+package com.abmedia.hsfuldapp;
+
+/**
+ * Created by Dini on 13.06.2017.
+ */
+
+public class Grade {
+
+
+        public String datum, pruefung, status, versuch, credits, note;
+
+        public Grade(String datum, String pruefung, String status, String versuch, String credits, String note){
+            this.datum = datum;
+            this.pruefung = pruefung;
+            this.status = status;
+            this.versuch = versuch;
+            this.credits = credits;
+            this.note = note;
+        }
+
+
+
+
+}
