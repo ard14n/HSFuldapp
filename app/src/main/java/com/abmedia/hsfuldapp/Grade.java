b81b20b011cf5b12ab96448e4ea60b5456b0797e
ard14n <ardianj4@gmail.com>
2017-06-13 20:41:42 +0200

    10.06 11:22

--- app/src/main/java/com/abmedia/hsfuldapp/Grade.java
+++ app/src/main/java/com/abmedia/hsfuldapp/Grade.java
@@ -1,5 +1,9 @@
 package com.abmedia.hsfuldapp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Dini on 13.06.2017.
  */
@@ -8,6 +12,7 @@ public class Grade {
 
 
         public String datum, pruefung, status, versuch, credits, note;
+        public HashMap<String, String> averageGrades;
 
         public Grade(String datum, String pruefung, String status, String versuch, String credits, String note){
             this.datum = datum;
@@ -17,6 +22,18 @@ public class Grade {
             this.credits = credits;
             this.note = note;
         }
+
+
+    public Grade(String datum, String pruefung, String status, String versuch, String credits, String note, HashMap<String, String> averageGrades){
+        this.datum = datum;
+        this.pruefung = pruefung;
+        this.status = status;
+        this.versuch = versuch;
+        this.credits = credits;
+        this.note = note;
+        this.averageGrades = averageGrades;
+    }
+
 
 
 
