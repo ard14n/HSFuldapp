3b2b63a6f2be1117c84109c3ac0d93cb8c2b4ba2
ard14n <ardianj4@gmail.com>
2017-06-13 20:42:09 +0200

    10.06 11:22

--- app/src/main/java/com/abmedia/hsfuldapp/Grade.java
+++ app/src/main/java/com/abmedia/hsfuldapp/Grade.java
@@ -14,14 +14,14 @@ public class Grade {
         public String datum, pruefung, status, versuch, credits, note;
         public HashMap<String, String> averageGrades;
 
-        public Grade(String datum, String pruefung, String status, String versuch, String credits, String note){
-            this.datum = datum;
-            this.pruefung = pruefung;
-            this.status = status;
-            this.versuch = versuch;
-            this.credits = credits;
-            this.note = note;
-        }
+    public Grade(String datum, String pruefung, String status, String versuch, String credits, String note){
+        this.datum = datum;
+        this.pruefung = pruefung;
+        this.status = status;
+        this.versuch = versuch;
+        this.credits = credits;
+        this.note = note;
+    }
 
 
     public Grade(String datum, String pruefung, String status, String versuch, String credits, String note, HashMap<String, String> averageGrades){
